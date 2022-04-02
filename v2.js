c4faa56aed7b7956d44936103b60637b7c9fccb1
John Skoteiniotis <j0hnskot@gmail.com>
2022-04-02 19:38:26 +0100

    stores "following"

--- v2.js
+++ v2.js
@@ -1,4 +1,5 @@
 const followDelay = 500;
+const delayForRefresh = 1500;
 const debug = true;
 
 const userCellQuery = 'div [data-testid*="UserCell"]';
@@ -6,12 +7,46 @@ const followerQuery = "//span[contains(., 'Follows you')]";
 const unfollowPopupQuery = "[data-testid='confirmationSheetConfirm']";
 const followButtonQuery = 'div [aria-label*="Follow "]';
 const alreadyFollowingQuery = 'div [aria-label*="Following @"]';
+const originalFollowingListQuery = 'div[data-testid*="-unfollow"]';
 const defaultTwitterImage = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png';
 
 start();
 
 async function start() {
   while (await follow());
+}
+
+async function saveOriginalFollowingList() {
+   
+    const originalFollowingList = [];
+   
+    let originalFollowingListDiv = []
+    
+    while((originalFollowingListDiv = document.querySelectorAll(originalFollowingListQuery)) && originalFollowingListDiv.length){
+ 
+        originalFollowingListDiv.forEach((e) => {
+            originalFollowingList.push(e.getAttribute('data-testid').split('-')[0]);
+            e.closest(userCellQuery).remove();
+        }); 
+
+        await sleep(delayForRefresh);
+    }
+
+    const valueToStore =  JSON.stringify(originalFollowingList)
+
+    if (localStorage.getItem('twitterFollowers') === null) {
+        localStorage.setItem('twitterFollowers', valueToStore); 
+    }
+
+    const c = document.createElement("a");
+    c.download = valueToStore;
+    const t = new Blob([valueToStore], {
+        type: "text/plain"
+    });
+    c.href = window.URL.createObjectURL(t);
+    c.click();
+    c.remove();
+
 }
 
 async function follow() {
@@ -78,7 +113,7 @@ async function removeAlreadyFollowed() {
       personDiv.closest(userCellQuery).remove();
     }
 
-    await sleep(1000);
+    await sleep(delayForRefresh);
   }
 
 }
