f0ca1700c4287e51e56839108873a30d59558641
John Skoteiniotis <j0hnskot@gmail.com>
2022-04-02 17:36:55 +0100

    basic version added

--- v2.js
+++ v2.js
@@ -1,50 +1,80 @@
-const sleep = (ms) => {
-    return new Promise((resolve, reject) => setTimeout(resolve, ms));
-};
-
-const followDelay = 500
+const followDelay = 500;
 const debug = true;
 
-// const toFollow = document.querySelectorAll('div [aria-label*="Follow"]')
-// document.querySelectorAll('div [aria-label*="Follow"]')
-// Could also do it with XPath - seems more efficient
-const xPathResult = document.evaluate("//span[contains(., 'Follows you')]", document, null, XPathResult.ANY_TYPE, null)
+start();
 
-const alreadyFollowing = [];
-let node = xPathResult.iterateNext();
-while (node) {
-    alreadyFollowing.push(node);
-  node = xPathResult.iterateNext();
+async function start() {
+  while (await follow());
 }
 
-alreadyFollowing.forEach(e =>{
-    e.closest('div [data-testid*="UserCell"]').remove();
-})
+async function follow() {
+  await removeAlreadyFollowing();
+  await removeAlreadyFollowed();
 
-const toFollow = document.querySelectorAll('div [data-testid*="UserCell"]')
+  const toFollow = document.querySelectorAll('div [data-testid*="UserCell"]');
 
-for(const personDiv of toFollow) {
-    console.log(personDiv)
+  if (toFollow.length === 0) {
+    return;
+  }
 
+  for (const personDiv of toFollow) {
     await sleep(followDelay);
     if (!debug) personDiv.querySelector('div [aria-label*="Follow "]').click();
-    const unfollowOpen = document.querySelector("[data-testid='confirmationSheetConfirm']")
-    if(unfollowOpen) unfollowOpen.click();
+
+    const unfollowPopupOpen = document.querySelector(
+      "[data-testid='confirmationSheetConfirm']"
+    );
+    if (unfollowPopupOpen) unfollowPopupOpen.click();
+
+    personDiv.remove();
+  }
+
+  return true;
 }
 
- 
+async function removeAlreadyFollowing() {
 
-function removeAlreadyFollowed() {
-    let alreadyFollowed = document.querySelectorAll('div [aria-label*="Following @"]');
-    while((alreadyFollowed = document.querySelectorAll('div [aria-label*="Following @"]')) && alreadyFollowed.length) {
-        for(const personDiv of alreadyFollowed) {
-            personDiv.closest('div [data-testid*="UserCell"]').remove();
-        }
+  const xPathResult = document.evaluate(
+    "//span[contains(., 'Follows you')]",
+    document,
+    null,
+    XPathResult.ANY_TYPE,
+    null
+  );
 
-        await sleep(1000);
+  const alreadyFollowing = [];
+  let node = xPathResult.iterateNext();
+  while (node) {
+    alreadyFollowing.push(node);
+    node = xPathResult.iterateNext();
+  }
 
+  alreadyFollowing.forEach((e) => {
+    e.closest('div [data-testid*="UserCell"]').remove();
+  });
+  
+}
+
+async function removeAlreadyFollowed() {
+
+  let alreadyFollowed = document.querySelectorAll(
+    'div [aria-label*="Following @"]'
+  );
+  while (
+    (alreadyFollowed = document.querySelectorAll(
+      'div [aria-label*="Following @"]'
+    )) &&
+    alreadyFollowed.length
+  ) {
+    for (const personDiv of alreadyFollowed) {
+      personDiv.closest('div [data-testid*="UserCell"]').remove();
     }
 
-    console.log('done')
+    await sleep(1000);
+  }
+
 }
 
+async function sleep(ms) {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms));
+}
