cf85d0192b49722e0087d2c5c057e62778ee8fd1
John Skoteiniotis <j0hnskot@gmail.com>
2022-04-02 23:44:37 +0100

    v2

--- main.js
+++ main.js
@@ -1,352 +1,311 @@
-Object.size = function (obj) {
-		var size = 0,
-				key;
-		for (key in obj) {
-				if (obj.hasOwnProperty(key)) size++;
-		}
-		return size;
+const UIRemovalDelay = 100;
+const followDelay = 500;
+const delayForRefresh = 1500;
+const randomDelay = 3000;
+const deepSleepDelay = 60000;
+
+let debug = true;
+
+const userCellQuery = 'div [data-testid*="UserCell"]';
+const followerQuery = "//span[contains(., 'Follows you')]";
+const unfollowPopupQuery = "[data-testid='confirmationSheetConfirm']";
+const followButtonQuery = 'div [aria-label*="Follow "]';
+const followingButtonQuery = 'div [aria-label*="Following @"]';
+const unfollowButtonIDQuery = 'div[data-testid*="-unfollow"]';
+const followButtonIDQuery = 'div[data-testid*="-follow"]';
+const defaultTwitterImage = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png';
+const rateLimitError =  '[href="https://support.twitter.com/articles/66885"]';
+
+let stopRunning = false;
+
+const storedFollowers = getStoredFollowers();
+
+const panelDiv = document.createElement('div');
+panelDiv.setAttribute('style', 'position: fixed; bottom: 0; left: 0; width: 100%; height: 50px; background-color: #f5f5f5;');
+document.body.appendChild(panelDiv);
+
+const saveFollowingBtn = document.createElement("button"); 
+saveFollowingBtn.innerText = "Save Original Following List";
+saveFollowingBtn.onclick = saveOriginalFollowingList;
+saveFollowingBtn.setAttribute('style', 'background-color: #fff; border: 1px solid #ccc; padding: 10px;');
+panelDiv.appendChild(saveFollowingBtn);
+
+const followBtn = document.createElement("button"); 
+followBtn.innerText = "Start Following";
+followBtn.onclick = start;
+followBtn.setAttribute('style', 'background-color: #fff; border: 1px solid #ccc; padding: 10px;');
+panelDiv.appendChild(followBtn);
+
+const unfollowBtn = document.createElement("button"); 
+unfollowBtn.innerText = "Start Unfollowing";
+unfollowBtn.onclick = unfollow;
+unfollowBtn.setAttribute('style', 'background-color: #fff; border: 1px solid #ccc; padding: 10px;');
+panelDiv.appendChild(unfollowBtn);
+
+const stopBtn = document.createElement("button"); 
+stopBtn.innerText = "Stop";
+stopBtn.onclick = _ => { stopRunning = true; };
+stopBtn.setAttribute('style', 'background-color: #ff00; border: 1px solid #ccc; padding: 10px;');
+panelDiv.appendChild(stopBtn);
+
+const debugButton = document.createElement("button"); 
+debugButton.innerText = "Debug is" + (debug ? " on" : " off");
+debugButton.setAttribute('style', 'background-color:' + (debug ? '#008000' : '#FF0000 ') + '; border: 1px solid #ccc; padding: 10px;');
+debugButton.onclick = () => {
+    debug = !debug;
+    debugButton.innerText = "Debug is" + (debug ? " on" : " off");
+    debugButton.setAttribute('style', 'background-color:' + (debug ? '#008000' : '#FF0000 ') + '; border: 1px solid #ccc; padding: 10px;');
 };
+panelDiv.appendChild(debugButton);
 
-var FOLLOW_DELAY = 500,
-		STOP = false,
-		messages = {
-				"errors": '',
-				"warnings": '',
-				"info": '',
-				"clear": function(){
+const deepSleepText = document.createElement("span"); 
+deepSleepText.innerText = "Deep Sleep is on - wait for " + deepSleepDelay / 1000 + " seconds";
+deepSleepText.setAttribute('style', 'color: white; background-color: #FF0000; border: 1px solid #ccc; padding: 10px;');
+deepSleepText.style.display = 'none';
+panelDiv.appendChild(deepSleepText);
 
-						messages.errors = '';
-						messages.warnings = '';
-						messages.info = '';
-
-					}
-		},
-		i = 0,
-		lastWindowHeight = 0,
-		scrollTries = 30,
-		names = {},
-		oldNames = {},
-		oldFollowers = {},
-		whiteList = ['21861033','16150760','8520','11417762','2389664502','35761106', '998182430', '192969516', '453510020', '16004473', '1114232789'];
-
-if (localStorage.getItem('twitterFollowers') === null) {
-		localStorage.setItem('twitterFollowers', JSON.stringify(names));
+async function start() {
+  while (!stopRunning && await follow());
+  stopRunning = false; 
 }
 
-oldNames = JSON.parse(localStorage.getItem('twitterFollowers'));
-
-function follow(force, mute) {
-
-		names = {};
-		i = 0;
-		STOP = false;
-		hideFollowersByButtonColor();
-		hideInactive();
-		hideLocked();
-		var idAttrName = 'data-user-id';
-
-
-		$('div.account').each(function (index) {
-				names[index] = $(this).attr('data-user-id');
-		});
-
-		if(Object.size(names)===0){
-			$('div[data-test-selector="ProfileTimelineUser"]').each(function (index) {
-					names[index] = $(this).children('div.js-stream-item').attr('data-item-id');
-			});
-			idAttrName = 'data-item-id';
-			addMessage('TEST2' ,'error');
-
-
-		}
-
-
-		addMessage(JSON.stringify(names) ,'error')
-		addMessage('TEST' ,'error')
-
-		if (localStorage.getItem('twitterFollowers') === null) {
-				localStorage.setItem('twitterFollowers', JSON.stringify(names));
-		} else {
-
-				var alreadyFollowed = true;
-
-				for (var name in names) {
-						alreadyFollowed = false;
-
-						for (var oldName in oldNames) {
-								if (!oldNames.hasOwnProperty(oldName) || alreadyFollowed) continue;
-								if (oldName === names[name]) {
-										alreadyFollowed = true;
-										continue;
-								}
-
-						}
-
-						if (!alreadyFollowed || (typeof force !== 'undefined' && force === true)) {
-
-								addMessage("Will follow: " + names[name]);
-								var q = $("div[" +idAttrName+"='" + names[name] + "']");
-
-								window.setTimeout(followCurrent.bind(this, names[name]), FOLLOW_DELAY * i + 1);
-
-								i += 1;
-
-						} else {
-								addMessage('already followed ' + names[name], 'warning');
-						}
-
-				}
-
-		}
-
-		function followCurrent (name) {
-					if(STOP) return;
-						var q = $("div["+idAttrName+"='" + name + "']");
-						$(q).find('button[class~=follow-button]').trigger('click');
-
-						saveFollower(name);
-						if (typeof mute !== 'undefined' && mute === true) muteFollower(q);
-
-				}
-
+function getOriginalFollowingList() {
+    const originalFollowingList = JSON.parse(localStorage.getItem('originalTwitterFollowingList'));
+    return originalFollowingList || [];
 }
 
-function muteVisible() {
+async function saveOriginalFollowingList() {
+    
+    if(!window.location.pathname.includes('/following')) {
+        window.location.href = 'https://twitter.com/following';
+        return
+    }
 
-		var i = 0;
-		$('div.account').each(function (index) {
+    const originalFollowingList = [];
+    let originalFollowingListDiv = []
+    
+    while((originalFollowingListDiv = document.querySelectorAll(unfollowButtonIDQuery)) && originalFollowingListDiv.length){
+ 
+        for (const e of originalFollowingListDiv) {
+            originalFollowingList.push(e.getAttribute('data-testid').split('-')[0]);
+            e.closest(userCellQuery).remove();
+            await sleep(UIRemovalDelay);
+        }
+    }
 
-				window.setTimeout(function (name) {
-								muteFollower($(this));
-						}
-						.bind(this), FOLLOW_DELAY * i + 1);
-				i++;
-		});
+    const valueToStore =  JSON.stringify(originalFollowingList)
+     
+    const  currentValue = JSON.parse(localStorage.getItem('originalTwitterFollowingList'));
+    if (currentValue === null || currentValue.length === 0) {
+        localStorage.setItem('originalTwitterFollowingList', valueToStore); 
+    }
 
+    saveStringToJSONFile(valueToStore);
 }
 
-function muteFollower(followerDiv) {
+async function follow() {
+  
+  stopIfNoOriginalList();
+  await removeAlreadyFollowing();
+  await removeAlreadyFollowed();
+  await removeInactive();
+  
+  const toFollow = document.querySelectorAll(userCellQuery);
 
-		var follower = $(followerDiv).find('div.user-actions').find('div.dropdown-menu').find('button:contains("Mute")').trigger('click');
+  if (toFollow.length === 0) {
+    return;
+  }
 
+  for (const personDiv of toFollow) {
+    if(stopRunning) return false;
+    
+    if (!debug) {
+       await pressFollowButton(personDiv);
+    }
+     
+    saveFollower(personDiv);
+    await sleep(followDelay);
+    personDiv.remove();
+  }
+
+  return true;
 }
 
-function saveFollower(followerID, resetDate) {
+async function pressFollowButton(personDiv) {
+   const button = personDiv.querySelector(followButtonQuery);
+   if(!button) return;
+   button.click();
 
-		oldNames[followerID] = {
-				"date": (resetDate)? 0 : Date.now()
-		};
-		localStorage.setItem('twitterFollowers', JSON.stringify(oldNames));
-		addMessage('Saved ' + followerID );
-
+   await sleep(followDelay)
+   while(checkForAPIRateLimitError()){
+       await enterDeepSleep();
+   }
 }
 
-function removeFromStorage(followerID) {
-
-		var names = JSON.parse(localStorage.getItem('twitterFollowers'));
-		if (typeof names[followerID] === 'undefined') {
-				addMessage(followerID + ' was not found', 'error');
-				return;
-		}
-
-		names[followerID] = undefined;
-		addMessage(names[followerID]);
-		localStorage.setItem('twitterFollowers', JSON.stringify(names));
-		addMessage(followerID + ' is deleted from localStorage');
-
-}
-//Adds time passed since following to each account div
-function filterByTime(days) {
-
-	var dayInMS = 86400000;
-	days = days || 1;
-	var i = 0;
-	addMessage('Starting size : ' + Object.size(oldNames));
-
-	for (var id in oldNames) {
-
-			if(Date.now() - oldNames[id].date < days * dayInMS){
-
-				window.setTimeout(removeCurrent.bind(this,id),10 * i + 1);
-				i+=1;
-			}
-
-	}
-
-	function removeCurrent (userId) {
-
-		$("div[data-test-selector=ProfileTimelineUser]").has("div[data-user-id='" + userId + "']").remove();
-
-		addMessage(userId + ' Is removed');
-
-	}
-
-	addMessage('Ending size: ' + i);
-
+async function enterDeepSleep() {
+    deepSleepText.style.display = 'inline';
+    await sleep(deepSleepDelay);
+    deepSleepText.style.display = 'none';
 }
 
-function findUnfollowers(filterByTime, days) {
+async function pressUnfollowButton(personDiv) {
+    const button =  personDiv.querySelector(unfollowButtonIDQuery);
+    if(!button) return;
+    button.click();
 
-		var dayInMS = 86400000;
-		days = days || 1;
-		var i = 0;
-		var currentName = '';
-		var unfollow = $('div[data-test-selector="ProfileTimelineUser"]:not(:has(span.FollowStatus))');
-		unfollow.each(function (q) {
+    await sleep(followDelay)
+    while(checkForAPIRateLimitError()){
+        await sleep(deepSleepDelay);
+    }
+}
 
-				window.setTimeout(function () {
+function checkForAPIRateLimitError() {
+    const error = document.querySelector(rateLimitError);
+    return error; 
+}
 
-						currentName = $(this).find('div[data-user-id]').attr('data-user-id');
-						addMessage(currentName,'error');
+async function unfollow() {
+    stopIfNoOriginalList();
 
-						if(typeof oldNames[currentName] === 'undefined'){ saveFollower(currentName); return;}
+    let alreadyFollowed = document.querySelectorAll(
+        followingButtonQuery
+    );
 
-						if(filterByTime === true && (Date.now() - oldNames[currentName].date < days * dayInMS) ){
-							addMessage('wtf ' + currentName,'error');
-							return;
-						}
+      while (
+        (alreadyFollowed = document.querySelectorAll(
+            followingButtonQuery
+        )) &&
+        alreadyFollowed.length
+        && !stopRunning
+      ) {
+        for (const unfollowBtn of alreadyFollowed) {
+          if(stopRunning) return false;
+        
+          const personDiv = unfollowBtn.closest(userCellQuery)
+          const personID = getPersonID(personDiv);
+          if(personID === null) continue;
+            
+          if(!isPersonIDInOriginalList(personID) && !debug) {
+            await pressUnfollowButton(personDiv);
+            const unfollowPopupOpen = document.querySelector(
+                unfollowPopupQuery
+              );
+            if (unfollowPopupOpen) unfollowPopupOpen.click();
+            await sleep(followDelay);
+          }
 
-						if (whiteList.indexOf(currentName) !== -1) {
-								addMessage('The user ' + currentName + ' is whitelisted', 'warning');
-								return;
-						}
+          personDiv.remove(); 
+        }
+    
+        await sleep(delayForRefresh);
+      }
+}
 
-						addMessage('unfollowed ' + currentName);
-						$(this).find('button.follow-button').trigger('click');
-						saveFollower(currentName, true);
-						// $(this).remove();
+function isPersonIDInOriginalList(personID) {
+    const originalFollowingList = getOriginalFollowingList();
+    if(originalFollowingList.includes(personID)){
+        console.log(`${personID} is in the original list`);
+        return true;
+    }
 
-				}.bind(this), FOLLOW_DELAY * i + 1);
-				i += 1;
+    return false;
+}
 
+async function removeAlreadyFollowing() {
 
-		});
+  const xPathResult = document.evaluate(
+    followerQuery,
+    document,
+    null,
+    XPathResult.ANY_TYPE,
+    null
+  );
+
+  const alreadyFollowing = [];
+  let node = xPathResult.iterateNext();
+  while (node) {
+    alreadyFollowing.push(node);
+    node = xPathResult.iterateNext();
+  }
+
+  for (const e of alreadyFollowing) {
+    e.closest(userCellQuery).remove();
+    await sleep(UIRemovalDelay);
+  };
 
 }
 
-function hideFollowers() {
+async function removeAlreadyFollowed() {
 
-
-		var i = 0;
-		var currentName = '';
-		var followers = $('div[data-test-selector="ProfileTimelineUser"]:has(span.FollowStatus)');
-		followers.each(function (q) {
-
-				window.setTimeout(function () {
-						currentName = $(this).find('div[data-screen-name]').attr('data-screen-name');
-						if (whiteList.indexOf(currentName) !== -1) {
-								return;
-						}
-						$(this).remove();
-
-				}.bind(this), 100 * (i + 1));
-				i += 1;
-
-		});
-
+  let alreadyFollowed = document.querySelectorAll(
+    followingButtonQuery
+  );
+  while (
+    (alreadyFollowed = document.querySelectorAll(
+        followingButtonQuery
+    )) &&
+    alreadyFollowed.length
+    && !stopRunning
+  ) {
+    for (const personDiv of alreadyFollowed) {
+      personDiv.closest(userCellQuery).remove();
+      await sleep(UIRemovalDelay);
+    }
+  }
 }
 
-function hideFollowersByButtonColor() {
+async function removeInactive() {
 
-		var q = [];
-		var a = $('li[data-item-type*="user"]');
-
-		if(a.length === 0){ // This works when following through another person's followers
-			a = $('div[data-test-selector="ProfileTimelineUser"');
-
-		}
-
-		a = a.has('span.following-text').filter(function () {
-				var button = $(this).find('span.following-text');
-				var color = $(button).css("border-color").toLowerCase();
-				q.push(color);
-				return color === "rgb(255, 255, 255)";
-		});
-
-		a.each(function () {
-				$(this).remove();
-		});
-
-}
-//checks if the user has the default photo
-function hideInactive() {
-
-		var a = $('li[data-item-type*="user"]').has('img[src*="abs.twimg.com/sticky/default_profile_images/"]');
-		a.each(function () {
-				$(this).remove();
-		});
-
+    const inactive = document.querySelectorAll('img[src*="' + defaultTwitterImage + '"]');
+    for (const e of inactive) {
+        e.closest(userCellQuery).remove();
+        await sleep(UIRemovalDelay);
+    }
 }
 
-function hideLocked() {
-
-		var a = $('li[data-item-type*="user"]')
-
-		if(a.length === 0){ // This works when following through another person's followers
-			a = $('div[data-test-selector="ProfileTimelineUser"');
-
-		}
-
-		a = a.has('span.Icon--protected');
-		a.each(function () {
-				$(this).remove();
-		});
-
+function getStoredFollowers() {
+    const storedFollowers = localStorage.getItem('twitterFollowers');
+    if (storedFollowers) {
+        return JSON.parse(storedFollowers);
+    }
+    return {};
 }
 
-function showOnlyVerified() {
-		var nonVerified = $('div.js-stream-item').not(":has('span.Icon--verified')");
-		$(nonVerified).each(function () {
-				this.remove();
-		});
+function saveFollower(followerDiv) {
+ 
+    const followerID = getPersonID(followerDiv)
+    storedFollowers[followerID] = {
+            "date":  Date.now()
+    };
+    localStorage.setItem('twitterFollowers', JSON.stringify(storedFollowers));
 }
 
-function scrollToBottom() {
+function getPersonID(personDiv) {
+    const followBtn = personDiv.querySelector(followButtonIDQuery) || personDiv.querySelector(unfollowButtonIDQuery);
+    
+    if(followBtn === null) return null;
 
-		if (scrollTries === 0) {
-				scrollTries = 30;
-				return;
-		}
-		if (document.body.scrollHeight === lastWindowHeight) {
-				scrollTries--;
-				window.setTimeout(scrollToBottom, 1000);
-				return;
-		}
-		lastWindowHeight = document.body.scrollHeight;
-		window.scrollTo(0, document.body.scrollHeight);
-		messages.errors += lastWindowHeight + '\n';
-		scrollTries = 30;
-		window.setTimeout(scrollToBottom, 600);
-
-
+    return followBtn.getAttribute('data-testid').split('-')[0];
 }
 
-function addMessage(message, reason) {
-
-		if (typeof reason === 'undefined') {
-				messages.info += "\n" + message + "   (" + new Date() + ")\n";
-				return;
-		}
-		if (reason === 'error') {
-				messages.errors += "\n" + message + "   (" + new Date() + ")\n";
-				return;
-		}
-
-		if (reason === 'warning') {
-				messages.warnings += "\n" + message + "   (" + new Date() + ")\n";
-				return;
-		}
-
-
-
+function stopIfNoOriginalList() {
+    if(!localStorage.getItem('originalTwitterFollowingList')) {
+        alert('No original follower list found. Please save the original list first.');
+        stopRunning = true;
+    }
 }
 
-function createWindow(){
-$("<style type='text/css'>#twitterFollowersWindow{color:#f00;	font-weight:bold;	font-size: 30px;	width:300px;	height:600px;	background: grey;	position: fixed;	top:0; z-index:99999} </style>").appendTo('head');
+function saveStringToJSONFile(valueToStore) {
+    const c = document.createElement("a");
+    c.download = valueToStore;
+    const t = new Blob([valueToStore], {
+        type: "application/json"
+    });
+    c.href = window.URL.createObjectURL(t);
+    c.click();
+    c.remove();
+}
 
-$('<div id="twitterFollowersWindow"><button follow-app-btn="follow" ">Follow</button><button follow-app-btn="findUnfollowers">UnFollow</button><button follow-app-btn="follow">Follow</button></div>').appendTo('body');
-
-$('button[follow-app-btn=follow]').on('click',function(){follow()})
-
-
+async function sleep(ms, extraRandomMS = false) {
+	return new Promise((resolve, reject) => setTimeout(resolve, ms + (extraRandomMS ? Math.random() * randomDelay : 0)));
 }
